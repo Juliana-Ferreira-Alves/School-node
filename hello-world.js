02e3f5b484c1bad6758b686506535c8057805042
Juliana Alves <analistajferreiraalves@gmail.com>
2023-03-15 22:46:00 -0300

    Primeiro commit

---
+++ hello-world.js
@@ -0,0 +1,15 @@
+console.log("primeiro Hello World!");
+
+console.log('Meu primeiro arquivo javascript no node!');
+
+const numero1 = 45;
+const numero2 = 46;
+
+const soma = ()=>{
+    return numero1 + numero2;
+};
+
+console.log('O resultado da soma é:' , soma());
+
+
+
---
+++ primeiro-arquivo-node.js
@@ -0,0 +1,14 @@
+console.log("primeiro Hello World!");
+
+const numero1=15;
+const numero2=15;
+
+const soma =  () => {
+    return numero1 + numero2;
+
+};
+
+console.log('O resultado da soma é:', soma());
+
+console.log('A data de hoje é:', new Date());
+
