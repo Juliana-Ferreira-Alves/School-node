5f33c23216fde7e3ef4854fdd68522331d5ca1b3
Juliana Alves <analistajferreiraalves@gmail.com>
2023-03-22 23:00:23 -0300

    projeto-school-node

---
+++ primeiro-projeto/dist/primeiro-arquivo.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('Primeiro arquivo typescript!');
+let numero1 = 5;
+let numero2 = 10;
+const texto = 'Texto 2';
+const textoResultado = 'Resultado: ';
+const resultado = numero1 + numero2;
+console.log(textoResultado + resultado);
