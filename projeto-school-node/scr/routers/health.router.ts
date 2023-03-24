09698453c4641a83f1eaac4767e636ca73c7be66
Juliana Alves <analistajferreiraalves@gmail.com>
2023-03-23 21:55:01 -0300

    Criando Rotas

---
+++ projeto-school-node/scr/routers/health.router.ts
@@ -0,0 +1,20 @@
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+    const healthCheck = { messager: 'Aplicação funcionando com sucesso!'};
+    res.send(healthCheck);
+
+});
+
+router.get('/check', (req:Request, res:Response) => {
+    const healthCheck = { messager: 'Aplicação está funcionando normalmente!'};
+    res.send(healthCheck);
+})
+
+export default router;
+
+
+
+//http://localhost:3000/health/check
